801255b322dec2d0992aab88606309bc50a7601f
Gabriel Vinicius de Deus Sousa <111321165+GabrielHalls@users.noreply.github.com>
2023-05-13 12:27:25 -0300

    não finalizado
    não finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,25 @@ import './App.css'
 
 function App() {
   const [input, setIput] = useState("")
+  const [cod, setCod] = useState([])
+  
+  const YOUR_APP_ID = "0c72a8e2"
+  const YOUR_APP_KEY = "afd2600c75b5c05f9d59b8f3e4bfa3b2"
 
-  const handleSubmit = (event)=>{
+  let handleSubmit = (event)=> {
    event.preventDefault()
    setIput("")
+
+ fetch(`https://api.edamam.com/search?q=${input}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`)
+   .then((ress)=> ress.json())
+   .then((data)=> {
+    setCod(data.hits)
+    
+   })
    
    
   }
-
+  console.log(cod)
   return (
     <div className='App'>
 
@@ -20,18 +31,24 @@ function App() {
         <h1>Food App</h1>
 
         <form onSubmit={handleSubmit}>
-
-
           <input onChange={(e)=> setIput(e.target.value)} type="text" value={input}/>
-          <button type='Submit'>Submit</button>
-
-
-
+          <button type='submit'>Submit</button>
         </form>
-          <h1>{input}</h1>
+        
+   
       </div>
+     <div className='container'> 
+     {
+          cod.map((lanche)=>( 
+            <Food 
+            name={lanche.recipe.label}
+            img ={lanche.recipe.image}
+            calorias={Math.floor(lanche.recipe.calories)}/> 
 
 
+          ))
+        }
+</div>
 
     </div>
 
@@ -40,3 +57,6 @@ function App() {
 }
 
 export default App
+
+
+/* PROJETO EM CONSTRUÇÃO */
--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,10 +1,10 @@
-export default function Food(){
+export default function Food({name, calorias, img}){
     return(
         <div className="card"> 
-            <img src="https://pontotel.com.br/wp-content/uploads/2022/05/imagem-corporativa.jpg" />
+            <img src= {img} />
             <div className="nome-lanche">
-            <h1>BigMac</h1>
-            <p>total caolorias: <span>5000</span></p>
+            <h1>{name}</h1>
+            <p>total caolorias: <span>{calorias}</span></p>
             <button>Buy</button>
 
             </div>
